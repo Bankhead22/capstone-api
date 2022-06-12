73ba0d2e9243e9c2476c53a985c7a679837b7230
Bankhead22 <djbankhead0@gmail.com>
2022-06-11 20:59:47 -0500

    game route

--- app/models/games.js
+++ app/models/games.js
@@ -1,35 +1,48 @@
 const mongoose = require('mongoose')
 
-const noteSchema = require('./note')
+// const noteSchema = require('./note')
 
-const gameSchema = new mongoose.Schema({
-  name: {
-    type: String
-    // required: true
+const gameSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String
+      // required: true
+    },
+    released: {
+      type: String
+      // required: true
+    },
+    id: {
+      type: String
+      // required: true
+    },
+    image: {
+      type: String
+      // required: true
+    },
+    inLibrary: {
+      type: Boolean
+      // required: true
+    },
+    type: {
+      type: String
+      // required: true
+    },
+
+    // Add a `notes` subdocument array of type noteSchema
+    // notes: [noteSchema],
+
+    // Add a `games` subdocument array of game references,
+    owner: {
+      // References use the type ObjectId
+      type: mongoose.Schema.Types.ObjectId,
+      // the name of the model to which they refer
+      ref: 'User'
+    }
   },
-  released: {
-    type: String
-    // required: true
-  },
-  id: {
-    type: String
-    // required: true
-  },
-  image: {
-    type: String
-    // required: true
-  },
-  // Add a `notes` subdocument array of type noteSchema
-  notes: [noteSchema],
-  // Add a `games` subdocument array of game references,
-  owner: {
-    // References use the type ObjectId
-    type: mongoose.Schema.Types.ObjectId,
-    // the name of the model to which they refer
-    ref: 'User'
+  {
+    timestamps: true
   }
-}, {
-  timestamps: true
-})
+)
 
 module.exports = mongoose.model('Game', gameSchema)
